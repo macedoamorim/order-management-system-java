86b8f969c3cd1d0c4c626d0c27bffe7a1ec8e692
Pedro Macedo Amorim <pedrok.amorim07@gmail.com>
2026-02-16 12:01:53 -0300

    Class Order created

--- entities/Order.java
+++ entities/Order.java
@@ -1,8 +1,77 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.enums.OrderStatus;
 
 public class Order {
 
+private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+	
+	private LocalDate moment;
+	private OrderStatus status;	
+	
+	private Client client;
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order(LocalDate moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public LocalDate getMoment() {
+		return moment;
+	}
+
+	public void setMoment(LocalDate moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+	
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	private Double total() {
+		
+		double sum = 0.0;
+		
+		for (OrderItem itm : items) {
+			sum =+ itm.subTotal();
+		}
+		
+		return sum;
+	}
+	
 }
