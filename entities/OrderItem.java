9562b5c84c1b402434ffad373be704ba750f61ab
Pedro Macedo Amorim <pedrok.amorim07@gmail.com>
2026-02-16 11:30:38 -0300

    Class OrderItem created

--- entities/OrderItem.java
+++ entities/OrderItem.java
@@ -2,4 +2,24 @@ package entities;
 
 public class OrderItem {
 
+	private Integer quantity;
+	private Product product;
+	
+	public OrderItem(Integer quantity, Product product) {
+		this.quantity = quantity;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public Double subTotal() {
+		return product.getPrice() * quantity;
+	}
+	
 }
